d1048a4b8e00725b46280d4a368874f91e79e019
Sandip Patel <sandip@knackroot.com>
2019-12-06 16:41:50 +0530

    Fix compile issues

--- include/fc/io/raw.hpp
+++ include/fc/io/raw.hpp
@@ -373,14 +373,14 @@ namespace fc {
         template<typename Stream, typename T>
         static inline void pack( Stream& s, const T& v, uint32_t _max_depth ) {
           FC_ASSERT( _max_depth > 0 );
-          fc::raw::pack( s, signed_int((int64_t)v), _max_depth - 1 );
+          fc::raw::pack( s, (int64_t)v, _max_depth - 1 );
         }
         template<typename Stream, typename T>
         static inline void unpack( Stream& s, T& v, uint32_t _max_depth ) {
           FC_ASSERT( _max_depth > 0 );
-          signed_int temp;
+          int64_t temp;
           fc::raw::unpack( s, temp, _max_depth - 1 );
-          v = (T)temp.value;
+          v = (T)temp;
         }
       };
 
