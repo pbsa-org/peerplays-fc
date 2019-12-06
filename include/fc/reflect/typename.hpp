6cc9534891db71b5a0692e61a29a7b1d520a238e
Sandip Patel <sandip@knackroot.com>
2019-12-06 14:48:12 +0530

    64 bits unsigned int changes

--- include/fc/reflect/typename.hpp
+++ include/fc/reflect/typename.hpp
@@ -70,9 +70,7 @@ namespace fc {
      } 
   };
 
-  struct signed_int;
   struct unsigned_int;
-  template<> struct get_typename<signed_int>   { static const char* name()   { return "signed_int";   } };
   template<> struct get_typename<unsigned_int>   { static const char* name()   { return "unsigned_int";   } };
 
 }
