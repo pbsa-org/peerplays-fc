c8dfe448009061aacb3e6865fd9fb9e1254c673c
blockc p <pravin.blockc@gmail.com>
2020-08-12 02:59:56 +0000

    Update websocket.hpp

--- include/fc/network/http/websocket.hpp
+++ include/fc/network/http/websocket.hpp
@@ -72,9 +72,6 @@ namespace fc { namespace http {
          void listen( uint16_t port );
          void listen( const fc::ip::endpoint& ep );
          void start_accept();
-         uint16_t get_listening_port();
-         void stop_listening();
-         void close();
 
       private:
          friend class detail::websocket_tls_server_impl;
